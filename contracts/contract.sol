bfbc61d7fea74bd1ed3d7c95c5ed450dc39174ad
Sanjeevvarmabettter <sanjeevvarmacode@gmail.com>
2024-01-25 22:08:00 +0530

    compile the contract and deploy

---
+++ contracts/contract.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+contract coffee {
+    struct Memos {
+        string name;
+        //this can accept any amount
+        string message;
+        uint timestamp;
+        address from;
+    }
+
+    Memos[] private memos;
+    address payable owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function buymecoffee(
+        string calldata name,
+        string calldata message
+    ) external payable {
+        require(msg.value > 0, "You need to pay more eth for coffee");
+        owner.transfer(msg.value);
+        //after paying we need to push the entry into the memos array
+        memos.push((Memos(name, message, block.timestamp, msg.sender)));
+    }
+
+    //for getting the memos
+
+    function getmemos() public view returns (Memos[] memory) {
+        return memos;
+    }
+}
